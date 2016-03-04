0786f4c34633c65299705a2ab1ad90b9d0d8c8f8
Matt Hughes <matthughes72@gmail.com>
2016-03-04 15:55:50 -0500

    First Main Commit

---
+++ plot1.R
@@ -0,0 +1,20 @@
+plot1 <- function() {
+  nrows=2000
+  library("plyr")
+  
+  NEI <- readRDS("exdata-data-NEI_data/summarySCC_PM25.rds")
+  #SCC <- readRDS("exdata-data-NEI_data/Source_Classification_Code.rds")
+
+  #Easily tranform ONE column into a factor:
+  data<-transform(NEI,year=factor(year))
+  
+  #This is a terrific function to add up column numbers:
+  plotdata<-ddply(data,.(year),summarize,sum=sum(Emissions))
+  
+  png("plot1.png")
+  plot(plotdata$year,plotdata$sum,type="b",xlab="Year",ylab="Emission Totals", col="Red")
+  dev.off()
+  
+  return(plotdata)
+  
+}
