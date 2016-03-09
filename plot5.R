41f1df360764b28e1c2fb54da9f9d026e51ac155
Matt Hughes <matthughes72@gmail.com>
2016-03-09 17:48:56 -0500

    Plot 5

--- plot5.R
+++ plot5.R
@@ -1,15 +1,17 @@
 plot5 <- function() {
   
-  library("dplyr")
-  library("plyr")
+  library ("dplyr")
   library ("ggplot2")
   
+  #Read data
   NEI <- readRDS("exdata-data-NEI_data/summarySCC_PM25.rds")
   SCC <- readRDS("exdata-data-NEI_data/Source_Classification_Code.rds")
   
+  #Merge 2 datasets
   merged <- merge(NEI, SCC, by="SCC")
   
-  y<-filter(merged, grepl('[Vv]ehicles', merged$Short.Name)|
+  #Get all data related to Vehicle emissions
+  filtered<-filter(merged, grepl('[Vv]ehicles', merged$Short.Name)|
               grepl('[Vv]ehicles', merged$EI.Sector)|
               grepl('[Vv]ehicles', merged$SCC.Level.One)|
               grepl('[Vv]ehicles', merged$SCC.Level.Two)|
@@ -17,17 +19,21 @@ plot5 <- function() {
               grepl('[Vv]ehicles', merged$SCC.Level.Four)
   )
   
-  y = subset(y, fips=="24510", select=c(Emissions, year)) 
-  plotdata<-ddply(y, c("year"),summarize,sum=sum(Emissions))
+  #Get subset with only Baltimore emmisions
+  subset = subset(filtered, fips=="24510", select=c(Emissions, year)) 
   
+  #Create data to plot graph
+  plotdata <- aggregate(Emissions ~ year, data=subset, FUN=sum)
+  
+  #Plot graph
   png("plot5.png")
-  p<-qplot(year, sum, data=plotdata, geom="point", size=I(5), alpha=I(.5), 
-           main="Motor Vehicle Related Sources - Baltimore City", xlab="Year", ylab="Emissions")
+  p<-ggplot(plotdata, aes(x=factor(year), y=Emissions)) +
+    geom_bar(stat="identity") +
+    xlab("Year") +
+    ylab(expression("PM"[2.5]*" Emissions")) +
+    ggtitle("Vehicle Emissions in Baltimore City")
   print(p)
   dev.off()
-  
-  
-  return(y)
   
 }
 
