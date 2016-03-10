7ae081717202ea3b93d7c5f9d07f70c5e4aaca57
Matt Hughes <matthughes72@gmail.com>
2016-03-10 09:45:44 -0500

    Final

--- plot6.R
+++ plot6.R
@@ -1,14 +1,17 @@
 plot6 <- function() {
   
-  library("dplyr")
   library("plyr")
+  library("dplyr")
   library ("ggplot2")
   
+  #Get data
   NEI <- readRDS("exdata-data-NEI_data/summarySCC_PM25.rds")
   SCC <- readRDS("exdata-data-NEI_data/Source_Classification_Code.rds")
   
+  #Merge the 2 data sets
   merged <- merge(NEI, SCC, by="SCC")
   
+  #Filter new dataset based on the term 'Vehicles'
   y<-filter(merged, grepl('[Vv]ehicles', merged$Short.Name)|
               grepl('[Vv]ehicles', merged$EI.Sector)|
               grepl('[Vv]ehicles', merged$SCC.Level.One)|
@@ -17,22 +20,33 @@ plot6 <- function() {
               grepl('[Vv]ehicles', merged$SCC.Level.Four)
   )
   
-  y<-transform(y,fips=factor(fips))
-  y = subset(y, fips=="24510" | fips=="06037", select=c(Emissions, year, fips)) 
+  #Filter for the appropriate County (fips)
+  la = subset(y, fips=="24510", select=c(Emissions, year, fips)) 
+  baltimore = subset(y, fips=="06037", select=c(Emissions, year, fips)) 
   
-  rename(y, c("fips"="City"))
-  plotdata<-ddply(y, c("year", "fips"),summarize,sum=sum(Emissions))
+  #Aggragate 2 data sets
+  la <- aggregate(Emissions ~ year, data=la, FUN=sum)
+  baltimore <- aggregate(Emissions ~ year, data=baltimore, FUN=sum)
   
-
+  #Give the fips appropriate County names
+  la$County <- "Los Angeles County, CA"
+  baltimore$County <- "Baltimore City, MD"
   
-#   png("plot6.png")
-#   p<-qplot(year, sum, data=plotdata, geom="point", color=fips, size=I(5), alpha=I(.5), 
-#            main="Motor Vehicle Related Sources", xlab="Year", ylab="Emissions")
-#   print(p)
-#   dev.off()
+  #Merge 2 Countyy datasets
+  merged <- rbind(la, baltimore)
   
+  #Draw Graph
+ png("plot6.png")
+  p<-ggplot(merged, aes(x=factor(year), y=Emissions, fill=County)) +
+    geom_bar(stat="identity") + 
+    facet_grid(County  ~ ., scales="free") +
+    ylab("Emissions") + 
+    xlab("Year") +
+    ggtitle(expression("Motor vehicle Emissions in Baltimore and Los Angeles"))
+  print(p)
+  dev.off()
   
-  return(plotdata)
+  #return(y)
   
 }
 
